5e29c6513f2caa24fb7e18b6e33d4e5308f35e79
Paweł Recław <pawel.reclaw@gmail.com>
2023-01-15 15:57:44 +0100

    Koniec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { ContentComponent } from './content/content.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { ContentSettingsComponent } from './content-settings/content-settings.component';
+import { ContentSignInComponent } from './content-sign-in/content-sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     ContentComponent,
-    NavigatorComponent
+    NavigatorComponent,
+    ContentSettingsComponent,
+    ContentSignInComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
